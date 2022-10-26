db0c23e6746f48d19b01ed3c7798220fbe42af45
itisj <63428885+itisj@users.noreply.github.com>
2022-10-26 22:32:14 +1100

    new design

--- sketch.js
+++ sketch.js
@@ -3,20 +3,20 @@ let cam2
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight*5);
   cam1 = createCapture(VIDEO);
   // cam1.size();
   cam1.hide();
   cam2 = createCapture(VIDEO);
   cam2.hide();
 
-  let div1 = createDiv('<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d278.6285144749442!2d144.96580344561502!3d-37.8059355681024!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642cdc133add5%3A0x74d384e7855d54ee!2sRMIT%20Building%2057!5e0!3m2!1sen!2sau!4v1665506136819!5m2!1sen!2sau" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>');
-  div1.style('z-index: 1');
-  div1.position(200, 200);
+  // let div1 = createDiv('<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d278.6285144749442!2d144.96580344561502!3d-37.8059355681024!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642cdc133add5%3A0x74d384e7855d54ee!2sRMIT%20Building%2057!5e0!3m2!1sen!2sau!4v1665506136819!5m2!1sen!2sau" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>');
+  // div1.style('z-index: 1');
+  // div1.position(200, 200);
 
-  let div2 = createDiv('<iframe width="560" height="315" src="https://www.youtube.com/embed/nwibL__1Ux4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
-  div2.style('z-index: -1');
-  div2.position(800, 100);
+  let div2 = createDiv('<iframe style="height: 300vh; width: 101vw;" src="https://www.bing.com/news" allowfullscreen></iframe>');
+  div2.style('z-index: -2');
+  div2.position(0, 0);
 }
 
 function snapme() {
@@ -36,13 +36,13 @@ function snapme() {
 
 
 function draw() {
-  background(0, 0, 0, 0.6) 
+  background(255, 255, 255, 0) 
   // the alpha value here is a trick i came up after several experiments, 
   // to save the trace of previous snaps
   // it might be another ways to achieve this function, 
   // but I think this method works particularly well artistically and visually,
   // with the effect of film pictures, if you pick the right value
-  image(cam1,0, 0, 320, 240);
+  // image(cam1, 0, 0, 320, 240);
   // live reference
   snapme();
 }
